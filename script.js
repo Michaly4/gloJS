2f68f566eb8ee95bbab347a913e99fe662dbd9d3
Michaly4 <ZaritskyiIvan@gmail.com>
2022-01-28 10:35:13 +0100

    add variable to Script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-//alert('Hallo Alert!');
-console.log('Hallo Console');
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert('Hey Alert');
+console.log('Hey Console');
