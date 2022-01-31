7c26fb83a7d2a7517c2be63955b3b4ebe9d2e593
Michaly4 <ZaritskyiIvan@gmail.com>
2022-01-31 19:23:38 +0100

    lesson2 prep

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ let screenPrice;
 let rollback;
 let fullPrice;
 let adaptive;
-
 //! ************************
 //alert('Hey Alert');
 //console.log('Hey Console');
-
+screenPrice = 444;
+rollback = 99;
+fullPrice = 50000;
+screens = 'Простые,Сложные,Интерактивные  ';
+title= 'ProjektJS';
+let percent = 50000*(99/100);
+//! *************************
+console.log(title,screens,fullPrice,rollback,screenPrice);
+console.log(screens.length);
+console.log(screens.toLowerCase().split());
+console.log(`Стоимость верстки экранов ${screenPrice} рублей ` );
+console.log(`Стоимость разработки сайта ${fullPrice} рублей  `);
+//console.log(`Процент отката посреднику за работу`,` ${fullPrice} * (${rollback}/100)`);
+console.log(`Процент отката посреднику за работу ${percent}`);
