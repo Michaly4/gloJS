09d40f274735a310cb7dca000200172130ed51bb
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-01 14:28:41 +0100

    ende  prep

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ let screenPrice = 252;
 let rollback = 54;
 let fullPrice = 52000;
 let adaptive = false;
-/* let str = "Hallo my friends";
-console.log(str.split(" ")); */
+
 //! ************************
 
 console.log(typeof title, typeof fullPrice,typeof rollback);
 console.log(screens.length);
 console.log(screens.toLowerCase().split(" "));
-/* console.log(screens.split(" ")); */
 console.log(`Стоимость верстки экранов ${screenPrice} рублей ` );
 console.log(`Стоимость разработки сайта ${fullPrice} рублей  `);
 console.log(`Процент отката посреднику за работу (fullPrice * (rollback/100))`);
