57ace93c37e1ac607cdac8356a9cf9849c5803ee
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-01 14:25:55 +0100

    ende korrektur prep

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let title = 'ProjektJS';
-let screens = 'Простые,Сложные,Интерактивные  ';
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 252;
 let rollback = 54;
 let fullPrice = 52000;
 let adaptive = false;
+/* let str = "Hallo my friends";
+console.log(str.split(" ")); */
 //! ************************
 
 console.log(typeof title, typeof fullPrice,typeof rollback);
 console.log(screens.length);
 console.log(screens.toLowerCase().split(" "));
+/* console.log(screens.split(" ")); */
 console.log(`Стоимость верстки экранов ${screenPrice} рублей ` );
 console.log(`Стоимость разработки сайта ${fullPrice} рублей  `);
 console.log(`Процент отката посреднику за работу (fullPrice * (rollback/100))`);
