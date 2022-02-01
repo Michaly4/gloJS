8261ecb1fbbb59538f08e581b066f217da68e98f
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-01 22:36:52 +0100

    Third try prep

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let title = 'ProjektJS';
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 252;
-let rollback = 54;
+let rollback = 4;
 let fullPrice = 52000;
 let adaptive = false;
 
@@ -9,7 +9,8 @@ let adaptive = false;
 
 console.log(typeof title, typeof fullPrice,typeof rollback);
 console.log(screens.length);
-console.log(screens.toLowerCase().split(" "));
+console.log(screens.toLowerCase().split(","));
 console.log(`Стоимость верстки экранов ${screenPrice} рублей ` );
 console.log(`Стоимость разработки сайта ${fullPrice} рублей  `);
-console.log(`Процент отката посреднику за работу (fullPrice * (rollback/100))`);
+console.log(`Процент отката посреднику за работу` + " " + fullPrice * (rollback / 100));
+
