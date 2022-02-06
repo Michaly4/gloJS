85d563299764ea83a1687f72050b3775c6166aaf
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-06 23:17:38 +0100

    korrekturEnde  lesson003

--- script.js
+++ script.js
@@ -11,25 +11,17 @@ let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = fullPrice - (fullPrice * rollback / 100);
 
 
-if (fullPrice >= 30000){
-   console.log("Даем скидку в 10%"); 
-}
-else if(fullPrice >= 15000 && fullPrice < 30000){
+if (fullPrice >= 30000) {
+   console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
    console.log("Даем скидку в 5%");
-}
-else if(fullPrice < 15000 && fullPrice > 0){
+} else if (fullPrice < 15000 && fullPrice > 0) {
    console.log("Скидка не предусмотрена");
-}
-else if(fullPrice <= 0){
+} else if (fullPrice <= 0) {
    console.log("Что то пошло не так");
 }
 
 
-let allServicePrices = function getAllServicePrices() {
-   return service2 + service1;
-   
-}
-console.log(getAllServicePrices());
 
 
 
@@ -46,11 +38,10 @@ console.log(servicePercentPrice);
 
 
 
+
 /* console.log(typeof title, typeof fullPrice,typeof rollback);
 console.log(screens.length);
 console.log(screens.toLowerCase().split(","));
 console.log(`Стоимость верстки экранов ${screenPrice} рублей ` );
 console.log(`Стоимость разработки сайта ${fullPrice} рублей  `);
 console.log(`Процент отката посреднику за работу` + " " + fullPrice * (rollback / 100)); */
-
-
