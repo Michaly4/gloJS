0bede651ae3821b44ed94160a0ae6270610a8a39
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-06 23:51:46 +0100

    add prep lesson04

--- script.js
+++ script.js
@@ -11,20 +11,73 @@ let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = fullPrice - (fullPrice * rollback / 100);
 
 
-if (fullPrice >= 30000) {
-   console.log("Даем скидку в 10%");
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-   console.log("Даем скидку в 5%");
-} else if (fullPrice < 15000 && fullPrice > 0) {
-   console.log("Скидка не предусмотрена");
-} else if (fullPrice <= 0) {
-   console.log("Что то пошло не так");
+// **** start Function ****
+
+function getAllServicePrices() {
+
+   return servicePrice1 + servicePrice2
 }
 
+let allServicePrices = getAllServicePrices();
+
+//console.log(allServicePrices);
+
+//******************************
+
+fullPrice = function getFullPrice() {
+   // body...
+   return screenPrice + allServicePrices;
+}
+fullPrice();
+// console.log(fullPrice ());
+
+//  ****************************
+function getTitle(str) {
+   // body...
+   //if(!str.trim())
+   if (str !== "" && str.charAt(0) !== " ") {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+   }
+}
+getTitle(title);
+// console.log(getTitle(title));
+
+//******************************
+servicePercentPrice = function getServicePercentPrices() {
+   // body...
+   return Math.ceil(fullPrice() - (fullPrice() * rollback / 100));
+}
+//console.log("?" + servicePercentPrice());
+
+// ********************************
+function getRollbackMessage() {
+   // body...
+   if (fullPrice() > 30000) {
+      console.log("Даем скидку в 10%");
+   } else if (fullPrice() > 15000 && fullPrice() < 30000 || fullPrice() == 30000) {
+      console.log("Даем скидку в 5%");
+   } else if (fullPrice() <= 15000) {
+      console.log('Скидка не предусмотрена');
+   } else if (fullPrice() <= 0) {
+      console.log("Что-то пошло не так,вы в пролёте");
+   }
+}
+
+// ******************************
+
+function showTypeOf() {
+   console.log(screens.toLowerCase().split(","));
+   getRollbackMessage();
+   console.log(servicePercentPrice());
+
+}
+showTypeOf();
 
 
 
 
+
+/*
 console.log(title.toUpperCase());
 console.log(screens.toLowerCase().split(","));
 console.log(screenPrice);
@@ -35,7 +88,7 @@ console.log(service2);
 console.log(servicePrice2);
 console.log(fullPrice);
 console.log(servicePercentPrice);
-
+*/
 
 
 
